47a90a5a6f3cb4e79bf504b50a4ceebd6f46cacb
HwangJunha <45688526+HwangJunha@users.noreply.github.com>
2021-10-27 11:53:34 +0900

    오픈채팅방

--- src/programmers/lever1/skill/Soultion2.java
+++ src/programmers/lever1/skill/Soultion2.java
@@ -1,5 +1,25 @@
 package programmers.lever1.skill;
 
-public class Soultion2 {
+import java.util.Arrays;
 
+public class Soultion2 {
+	 public String[] solution(String[] strings, int n) {
+		 String[] answer = {};
+		 answer = new String[strings.length]; //배열의 길이 만큼 증가
+
+		 for (int i = 0; i < strings.length; i++) {
+			 String point = strings[i].substring(n, n + 1); //포인트를 기준으로 정렬 
+			 strings[i] = point + strings[i]; //포인트 추가
+		 }
+
+		 Arrays.sort(strings); //정렬
+
+		 for (int i = 0; i < strings.length; i++) {
+			 answer[i] = strings[i].substring(1); //포인트를 제거하고 answer값 대입
+		 }
+
+
+
+		 return answer;
+	 }
 }
---
+++ src/programmers/lever2/OpenChatRoom.java
@@ -0,0 +1,55 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42888
+//오픈채팅방
+package programmers.lever2;
+
+import java.util.*;
+
+public class OpenChatRoom {
+	public String[] solution(String[] record) {
+        String[] answer = {};
+        ArrayList<String> list = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        
+        for(int i=0; i<record.length; i++){
+            StringTokenizer st = new StringTokenizer(record[i]," ");
+            String commend = st.nextToken();
+            String id = st.nextToken();
+             
+            
+            //enter, leave, change 
+            if(commend.equals("Enter")){
+                String nick = st.nextToken();
+                list.add(id+" "+"Enter");
+                map.put(id,nick);
+            }else if(commend.equals("Leave")){
+                list.add(id+" "+"Leave");
+            }else{
+                String nick = st.nextToken();
+                map.put(id,nick);
+            }
+        }
+        
+        //값 최종 변환
+        answer = new String[list.size()];
+        int count=0;
+        for(String e: list){
+            StringTokenizer st = new StringTokenizer(e," ");
+            String temp="";
+            String id = st.nextToken();
+            String commend = st.nextToken();
+            temp+=map.get(id);
+            
+            if(commend.equals("Enter")){
+                temp+="님이 들어왔습니다.";
+            }else{
+                temp+="님이 나갔습니다.";
+            }
+            
+            answer[count]=temp;
+            count++;
+        }
+        
+            
+        return answer;
+    }
+}
